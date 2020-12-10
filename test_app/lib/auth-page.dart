79d3118943c77b5440a15193c878d90eb79fd3d1
e-oj <eoo5498@rit.edu>
2020-12-10 05:42:39 -0500

    Refactored

--- test_app/lib/auth-page.dart
+++ test_app/lib/auth-page.dart
@@ -69,7 +69,6 @@ class AuthPage extends StatelessWidget {
       );
 
       case 'google': return GoogleAuth().visa.authenticate(
-          newSession: true,
           clientID: '463257508739-c03fcu5pej7odrci1tclk53qdd''tsa0vo.apps.googleusercontent.com',
           redirectUri: 'https://www.e-oj.com/oauth',
           state: 'googleAuth',
