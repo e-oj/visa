fec7abbf169f2e162a25253847f83f471337add9
e-oj <eoo5498@rit.edu>
2021-08-09 00:10:18 -0300

    Google, LinkedIn, and Spotify button branding in test app

--- test_app/lib/registration.dart
+++ test_app/lib/registration.dart
@@ -13,6 +13,7 @@ class RegistrationPage extends StatefulWidget {
 class _RegistrationPageState extends State<RegistrationPage> {
   Widget build(BuildContext context) {
     AppScale scale = AppScale(context);
+    final double iconWidth = scale.ofHeight(0.0245);
 
     return Scaffold(
       appBar: Utils.getAppBar(context, canGoBack: false),
@@ -26,53 +27,58 @@ class _RegistrationPageState extends State<RegistrationPage> {
                       0, scale.ofHeight(0.027), 0, scale.ofHeight(0.027)),
                   child: Column(children: [
                     Utils.getButton(
-                        text: 'Sign up with Twitch',
-                        color: HexColor('#6441A4'),
-                        textColor: Colors.white,
-                        icon: Image.asset('assets/twitch.png',
-                            width: scale.ofHeight(0.0245)),
-                        onPressed: () =>
-                            Navigator.pushNamed(context, '/twitch-auth')),
-                    Utils.getButton(
                         text: 'Sign up with Facebook',
                         color: HexColor('#4267B2'),
                         textColor: Colors.white,
-                        icon: Image.asset('assets/fb.png',
-                            width: scale.ofHeight(0.0245)),
+                        icon: Image.asset('assets/fb.png', width: iconWidth),
                         onPressed: () =>
                             Navigator.pushNamed(context, '/fb-auth')),
+                    Utils.getButton(
+                        text: 'Sign up with Google',
+                        color: Colors.white,
+                        textColor: HexColor('#4285F4'),
+                        icon:
+                        Image.asset('assets/google.png', width: iconWidth),
+                        onPressed: () =>
+                            Navigator.pushNamed(context, '/google-auth')),
+                    Utils.getButton(
+                        text: 'Sign up with Twitch',
+                        color: HexColor('#6441A4'),
+                        textColor: Colors.white,
+                        icon:
+                            Image.asset('assets/twitch.png', width: iconWidth),
+                        onPressed: () =>
+                            Navigator.pushNamed(context, '/twitch-auth')),
                     Utils.getButton(
                         text: 'Sign up with Discord',
                         color: HexColor('#7289DA'),
                         textColor: Colors.white,
-                        icon: Image.asset('assets/discord.png',
-                            width: scale.ofHeight(0.0245)),
+                        icon:
+                            Image.asset('assets/discord.png', width: iconWidth),
                         onPressed: () =>
                             Navigator.pushNamed(context, '/discord-auth')),
                     Utils.getButton(
                         text: 'Sign up with Github',
                         color: HexColor('#211F1F'),
                         textColor: Colors.white,
-                        icon: Image.asset('assets/github.png',
-                            width: scale.ofHeight(0.0245)),
+                        icon:
+                            Image.asset('assets/github.png', width: iconWidth),
                         onPressed: () =>
                             Navigator.pushNamed(context, '/github-auth')),
                     Utils.getButton(
-                        text: 'Sign up with Google',
-                        color: HexColor('#4285F4'),
-                        textColor: Colors.white,
-                        onPressed: () =>
-                            Navigator.pushNamed(context, '/google-auth')),
-                    Utils.getButton(
                         text: 'Sign up with LinkedIn',
-                        color: HexColor('#4285F4'),
+                        color: HexColor('#2867B2'),
                         textColor: Colors.white,
+                        icon: Image.asset('assets/linkedin.png',
+                            width: iconWidth + 2),
                         onPressed: () =>
                             Navigator.pushNamed(context, '/linkedin-auth')),
                     Utils.getButton(
                         text: 'Sign up with Spotify',
-                        color: HexColor('#4285F4'),
+                        color: HexColor('#1DB954'),
                         textColor: Colors.white,
+                        icon:
+                            Image.asset('assets/spotify.png', width: iconWidth),
                         onPressed: () =>
                             Navigator.pushNamed(context, '/spotify-auth'))
                   ])))),
