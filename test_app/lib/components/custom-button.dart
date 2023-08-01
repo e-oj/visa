2d8f12f207497c2cd21b6b1835b125bbc84d19d0
e-oj <ooolaojo@gmail.com>
2023-08-01 16:36:14 -0300

    Update custom button for test app

--- test_app/lib/components/custom-button.dart
+++ test_app/lib/components/custom-button.dart
@@ -3,47 +3,51 @@ import '../app-scale.dart';
 
 class CustomButton extends StatelessWidget {
   CustomButton(
-      {Key key,
-      @required this.text,
-      @required this.onPressed,
+      {Key? key,
+      required this.text,
+      required this.onPressed,
       this.width,
       this.height,
-      this.color,
-      this.textColor,
+      required this.color,
+      required this.textColor,
       this.icon})
       : super(key: key);
 
-  final double width;
-  final double height;
+  final double? width;
+  final double? height;
   final Color color;
   final Color textColor;
-  final Widget icon;
+  final Widget? icon;
   final String text;
   final VoidCallback onPressed;
 
   Widget build(BuildContext context) {
-    double width = this.width;
-    double height = this.height;
+    double? width = this.width;
+    double? height = this.height;
     AppScale scale = AppScale(context);
     Text text =
         Text(this.text, style: TextStyle(fontSize: scale.ofHeight(0.019)));
 
-    if (this.width != null && this.width <= 1) {
-      width = scale.ofWidth(this.width);
+    if (this.width != null && this.width! <= 1) {
+      width = scale.ofWidth(this.width!);
     }
 
-    if (this.height != null && this.height <= 1) {
-      height = scale.ofHeight(this.height);
+    if (this.height != null && this.height! <= 1) {
+      height = scale.ofHeight(this.height!);
     }
 
     return Container(
         width: width,
         height: height,
         margin: EdgeInsets.all(scale.ofHeight(0.011)),
-        child: RaisedButton(
+        child: ElevatedButton(
           // borderSide: BorderSide(color: Colors.white),
-          color: color,
-          textColor: textColor,
+          style: ButtonStyle(
+              backgroundColor: MaterialStatePropertyAll<Color>(color),
+              textStyle: MaterialStatePropertyAll<TextStyle>(
+                  TextStyle(color: textColor)
+              ),
+          ),
           child: icon == null
               ? text
               : Row(
@@ -57,7 +61,7 @@ class CustomButton extends StatelessWidget {
                       ),
                       text
                     ]),
-          onPressed: onPressed,
+          onPressed: onPressed
         ));
   }
 }
